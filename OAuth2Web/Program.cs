53ffab923b7749b6dc43477dcbb849ab34419845
Lutonet <101110383+Lutonet@users.noreply.github.com>
2022-05-19 00:34:10 +0100

    Serilog Implemented

--- OAuth2Web/Program.cs
+++ OAuth2Web/Program.cs
@@ -1,12 +1,14 @@
 using Serilog;
+using Serilog.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+Log.Logger = new LoggerConfiguration().CreateBootstrapLogger();
 builder.Host.UseSerilog((ctx, lc) => lc
-    .WriteTo.Console());
+    .ReadFrom.Configuration(ctx.Configuration));
+//.WriteTo.Console());
 //.WriteTo.Seq("http://localhost:5341")); ;
 var app = builder.Build();
 
@@ -17,7 +19,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
