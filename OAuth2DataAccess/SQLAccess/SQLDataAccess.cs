652d57872da19e384530f4571b77b281d93cb575
Lutonet <101110383+Lutonet@users.noreply.github.com>
2022-05-19 13:43:15 +0100

    SQL Data access class prepared

---
+++ OAuth2DataAccess/SQLAccess/SQLDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuth2DataAccess.SQLAccess
+{
+    internal class SQLDataAccess : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OAuth2Web/Program.cs
+++ OAuth2Web/Program.cs
@@ -1,16 +1,44 @@
 using Serilog;
 using Serilog.Configuration;
+using Microsoft.OpenApi.Models;
+
+Log.Logger = new LoggerConfiguration().CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-Log.Logger = new LoggerConfiguration().CreateBootstrapLogger();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Lutonet OAuth2 API",
+        Description = "Api documentation for the Authenticator",
+        TermsOfService = new Uri("https://lutonet.org/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Contact",
+            Url = new Uri("mailto://admin@lutonet.org")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://lutonet.org/license")
+        }
+    });
+});
+
 builder.Host.UseSerilog((ctx, lc) => lc
     .ReadFrom.Configuration(ctx.Configuration));
-//.WriteTo.Console());
-//.WriteTo.Seq("http://localhost:5341")); ;
+
 var app = builder.Build();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -26,7 +54,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapRazorPages();
 app.MapControllers();
 
