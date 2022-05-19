d6111269c415a61105d1b7f2ad8f71169e33226c
Lutonet <101110383+Lutonet@users.noreply.github.com>
2022-05-19 22:06:30 +0100

    updated roles relationship and removed user update stored procedure

--- OAuth2Data/dbo/Tables/Roles.sql
+++ OAuth2Data/dbo/Tables/Roles.sql
@@ -2,5 +2,6 @@
 (
   [Id] INT NOT NULL PRIMARY KEY IDENTITY,
   [Name] NVARCHAR (256) NOT NULL, 
-    [ApplicationId] NVARCHAR(128) NOT NULL,
+    [ApplicationId] NVARCHAR(128) NOT NULL, 
+    CONSTRAINT [FK_Roles_ToApplications] FOREIGN KEY ([ApplicationId]) REFERENCES [Applications]([Id]),
 )
