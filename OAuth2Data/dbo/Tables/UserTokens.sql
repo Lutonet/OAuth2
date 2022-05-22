27a42ba66b6ddcc8cfaf4c5ac57a99f176ba5c9d
Lutonet <101110383+Lutonet@users.noreply.github.com>
2022-05-22 13:28:19 +0100

    Added Identity modes, Modified Database

---
+++ OAuth2Data/dbo/Tables/UserTokens.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[UserTokens]
+(
+  [UserId] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [LoginProvider] NVARCHAR(128) NOT NULL, 
+    [Name] NVARCHAR(128) NOT NULL, 
+    [Value] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_UserTokens_ToUsers] FOREIGN KEY (UserId) REFERENCES [Users]([Id])
+)
+
+GO
+
+CREATE INDEX [IX_UserTokens_LoginProvider] ON [dbo].[UserTokens] (LoginProvider)
+
+GO
+
+CREATE INDEX [IX_UserTokens_Name] ON [dbo].[UserTokens] ([Name])
---
+++ OAuth2Identity/Models/IdentityModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuth2Identity.Models
+{
+    public class IdentityModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string UserId { get; set; }
+        public bool ReturnUrl { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsLocked { get; set; }
+    }
+}
