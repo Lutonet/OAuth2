c770783bcb19316c2d88683762876ef306567671
Lutonet <101110383+Lutonet@users.noreply.github.com>
2022-05-23 01:55:44 +0100

    Added Auto mapper

--- OAuth2Data/dbo/Stored Procedures/spUser_GetById.sql
+++ OAuth2Data/dbo/Stored Procedures/spUser_GetById.sql
@@ -2,25 +2,7 @@
   @Id NVarchar(128)
 AS
 BEGIN
-  SELECT [Id], 
-         [Email], 
-         [EmailConfirmed], 
-         [PhoneNumber], 
-         [PhoneNumberConfirmed], 
-         [TwoFactorEnabled], 
-         [LockoutEnabled], 
-         [LockoutEndDateUtc], 
-         [FirstName], 
-         [LastName], 
-         [PublicName], 
-         [DateOfBirth], 
-         [IsDeveloper], 
-         [Facebook], 
-         [Twitter], 
-         [Microsoft], 
-         [WebPage], 
-         [AccountCreated], 
-         [AccountLocked]
+  SELECT [Email] 
   FROM [dbo].[Users]
   WHERE Id = @Id;
 END
---
+++ OAuth2DataAccess/Models/UserPublicModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuth2DataAccess.Models
+{
+    public class UserPublicModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public DateTime LockoutEndDateUtc { get; set; }
+        public int AccessFailedCount { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PublicName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsDeveloper { get; set; }
+        public string Facebook { get; set; }
+        public string Twitter { get; set; }
+        public string Microsoft { get; set; }
+        public string Google { get; set; }
+        public string WebPage { get; set; }
+        public DateTime Timestamp { get; set; }
+        public DateTime AccountCreated { get; set; }
+        public DateTime AccountLocked { get; set; }
+        public string LockedReason { get; set; }
+        public string LockedByAdminId { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
