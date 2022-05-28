1f74d414540417393b01c7d80c2c5c98ef0d89c2
Lutonet <101110383+Lutonet@users.noreply.github.com>
2022-05-29 00:12:45 +0100

    Helper API server part finished

--- OAuth2Web/Api/Help/Help.cs
+++ OAuth2Web/Api/Help/Help.cs
@@ -1,18 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OAuth2Web.Helpers;
+using Microsoft.Extensions.Localization;
+using OAuth2Web.Models;
 
 namespace OAuth2Web.Api.Help
 {
     [ApiController]
     public class Help : ControllerBase
     {
-        public Help()
+        private IUserHelpers _helper;
+        public IStringLocalizer<Help> _loc { get; set; }
+
+        public Help(IUserHelpers helper, IStringLocalizer<Help> loc)
+
         {
+            _helper = helper;
+            _loc=loc;
         }
 
         [HttpGet("api/help/{topic}")]
-        public async Task<IActionResult> OnGetAsync(string topic)
+        public IActionResult OnGet(string topic)
         {
-            return Ok(topic);
+            HelperModel helper = _helper.GetHelp(topic);
+            HelperModel result = new();
+            result.Header = _loc[helper.Header];
+            result.BodyText = _loc[helper.BodyText];
+            result.Name = helper.Name;
+            return Ok(result);
         }
     }
 }
