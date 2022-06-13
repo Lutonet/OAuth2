2fbce4d642ce31877493f73ef559ad20edc1d43c
Lutonet <101110383+Lutonet@users.noreply.github.com>
2022-06-14 00:26:46 +0100

    very little progress with OAuth2 - Token service start

---
+++ OAuth2Identity/Services/TokenService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuth2Identity.Services
+{
+    public class TokenService
+    {
+        public TokenService()
+        {
+        }
+
+        public string GenerateToken(string userId, string applicationId)
+        {
+            return $"{userId} {applicationId}";
+        }
+    }
+}
